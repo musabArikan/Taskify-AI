ab7a767e8cb457c278dd4c0e0f2ba2b7c439d808
Musab Arıkan <150172110+musabArikan@users.noreply.github.com>
2025-04-03 02:59:35 +0300

    Update taskifyAi.js

--- frontend/src/API/taskifyAi.js
+++ frontend/src/API/taskifyAi.js
@@ -24,7 +24,7 @@ class TokenService {
 class BaseApi {
   constructor() {
     this.api = axios.create({
-      baseURL: "http://localhost:5000/api",
+      baseURL: "https://taskifyai-0qmc.onrender.com/api",
       headers: {
         "Content-Type": "application/json",
       },
